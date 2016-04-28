53234a766fa51145d4303ea4f4a2f1f9beac3cde
cliffordru <clifford.gray@gmail.com>
2016-04-28 00:24:17 -0400

    Begin wiring up angular in task list

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,3 +1,12 @@
-export default function(){
-	
+export default function($scope){
+	$scope.todos = [
+	{
+		task: 'do dishes',
+		isCompleted: false
+	},
+	{
+		task: 'walk the dog',
+		isCompleted: true
+	}
+	];
 }
