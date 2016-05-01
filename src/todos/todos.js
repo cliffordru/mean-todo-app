9c93e95cf055d30ff5e5cd01a723ffdd2c10013f
cliffordru <clifford.gray@gmail.com>
2016-05-01 17:31:22 -0400

    Save and Delete - using lodash for delete to write less code

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export default function($scope){
 	// create object
 	let params = {
@@ -20,11 +22,16 @@ export default function($scope){
 	// ES6 syntax
 	$scope.onCompletedClick = todo => {
 		todo.isCompleted = !todo.isCompleted;
-	}
+	};
 
 	$scope.onEditClick = todo => {
 		todo.isEditing = true;
-	}
+		todo.updatedTask = todo.task;
+	};
+
+	$scope.onCancelClick = todo => {
+		todo.isEditing = false;
+	};
 
 	$scope.createTask = () => {
 		params.createHasInput = false;
@@ -32,10 +39,20 @@ export default function($scope){
 
 		// This would be alternative if did not have $watch logic below
 		//$scope.todos.push($scope.createTaskInput);
-	}
+	};
 
 	/* ES6 syntax, older is 
 		$scope.$watch('createTaskInput', function(val){...}) */
+
+	$scope.updateTask = (todo) => {
+		todo.task = todo.updatedTask;
+		todo.isEditing = false;
+	};
+
+	$scope.deleteTask = todoToDelete => {
+		// Using lodash t below is local var iterator
+		_.remove($scope.todos, t => t.task === todoToDelete.task);
+	};
 
 	$scope.$watch('createTaskInput', val => {
 		if(!val && params.createHasInput){
